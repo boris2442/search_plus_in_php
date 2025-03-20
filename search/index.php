47cdfdaa5aa1c8cff838b8d0ec4ea523982a50de
boris2442 <aubinborissimotsebo@gmail.com>
2025-03-20 01:53:26 +0100

    text

--- search/index.php
+++ search/index.php
@@ -1,13 +1,15 @@
 <?php
 
-@$search=$_GET['search'];
-@$valider=$_GET['valider'];
-if(isset($valider) && !empty(trim($search))){
+@$search = $_GET['search'];
+@$valider = $_GET['valider'];
+if (isset($valider) && !empty(trim($search))) {
     include "./connexion.php";
-    $sql="SELECT desg * FROM `avis2` WHERE desg like?";
-    $requete=$db->prepare($sql);
-
-
+    // $sql="SELECT desg * FROM `avis2` WHERE desg like?";
+    $sql = "SELECT * FROM `avis2` WHERE desg LIKE ?";
+    $requete = $db->prepare($sql);
+    $requete->execute(['%' . $search . '%']);
+    $resultats = $requete->fetchAll(PDO::FETCH_ASSOC);
+    $affcher = "yes";
 }
 
 
@@ -19,23 +21,27 @@ if(isset($valider) && !empty(trim($search))){
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <title>search</title>
 </head>
 
 <body>
     <form action="" method="get">
-        <input type="text" name="search" id="" placeholder="effectuer une recherche">
+        <input type="text" name="search" id="" placeholder="effectuer une recherche" value="<?php echo $search ?>">
         <input type="submit" value="Valider" name="Valider">
     </form>
-    <div id="result">
-        <div class="nbr">
-            2 messages trouves
+    <?php if (@$afficher === "yes") { ?>
+        <div id="result">
+            <div class="nbr">
+                <?php echo count($tab) . "" . (count($tab) > 1 ? "resultats" : "resultat") ?>
+            </div>
+            <ol>
+                <?php for ($i = 0; $i < count($tab); $i++) { ?>
+                    <li><?php echo $tab[$i]["desg"] ?></li>
+            </ol>
+        <?php } ?>
         </div>
-        <ol>
-            <li>Resultat1</li>
-        </ol>
-    </div>
-
+    <?php } ?>
 </body>
 
 </html>
